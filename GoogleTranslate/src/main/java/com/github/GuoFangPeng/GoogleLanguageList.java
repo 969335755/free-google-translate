98270bd8b2314d77363413b14d91cfa8bea9521c
guo <969335755@qq.com>
2021-02-15 21:59:24 +0800

    加了个有语言缩写的类

---
+++ GoogleTranslate/src/main/java/com/github/GuoFangPeng/GoogleLanguageList.java
@@ -0,0 +1,14 @@
+package com.github.GuoFangPeng;
+
+public class GoogleLanguageList {
+    public static final String Japanese ="ja";
+    public static final String English ="en";
+    public static final String Chinese_simplified ="zh-CN";
+    public static final String Chinese_Traditional ="zh-TW";
+    public static final String Korean ="ko";
+    public static final String French ="fr";
+    public static final String Spanish ="es";
+    public static final String German ="de";
+    public static final String Russian ="ru";
+    public static final String Arabic ="ar";
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.github.GuoFangPeng.GoogleLanguageList;
 import com.github.GuoFangPeng.GoogleTranslateUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         srctext = findViewById(R.id.ed_text);
         Button button = findViewById(R.id.btn_call_api);
 
-        GoogleTranslateUtil.defaulanguage="zh-CN";
+        //等于 GoogleTranslateUtil.default_target_language="zh-CN";
+        GoogleTranslateUtil.default_target_language= GoogleLanguageList.Chinese_simplified;
         GoogleTranslateUtil g=new GoogleTranslateUtil(this, (type, response) -> {
             mTxtResult.post(() -> {     //一定要post 回到主进程  方式随意  但一定要回到主进程
                 mTxtResult.setText(response);
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         });
         button.setOnClickListener(view -> {
             g.query(srctext.getText().toString());
+//            g.query(srctext.getText().toString(),GoogleLanguageList.English,GoogleLanguageList.Chinese_simplified);
         });
     }
 }
